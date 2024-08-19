20fe52da6fd767f0a67b941c673a137c4d617513
Cristian Beraha <cristian@cloudops.works>
2024-08-19 16:19:10 -0300

    feat: working directory changed to /tmp/{UUID}

--- dowload-github.tf
+++ dowload-github.tf
@@ -16,11 +16,11 @@ resource "null_resource" "release_download_gh_java" {
   }
 
   provisioner "local-exec" {
-    command = "${path.module}/scripts/gh-download-java.sh ${var.repository_owner} ${var.source_name} ${var.source_version} ${var.package_name} ${var.package_type} .work/${var.release_name}/${local.build_folder}/app.jar"
+    command = "${path.module}/scripts/gh-download-java.sh ${var.repository_owner} ${var.source_name} ${var.source_version} ${var.package_name} ${var.package_type} ${local.tmp_dir}/${var.release_name}/${local.build_folder}/app.jar"
   }
 
   provisioner "local-exec" {
-    command = "chmod +x .work/${var.release_name}/${local.build_folder}/app.jar"
+    command = "chmod +x ${local.tmp_dir}/${var.release_name}/${local.build_folder}/app.jar"
   }
 }
 
@@ -37,17 +37,17 @@ resource "null_resource" "release_download_gh_node" {
   }
 
   provisioner "local-exec" {
-    command = "${path.module}/scripts/gh-download-npm.sh ${var.repository_owner} ${var.source_name} ${var.source_version} ${var.package_name} ${var.package_type} .work/${var.release_name}/${local.build_folder}/source-app.tgz"
+    command = "${path.module}/scripts/gh-download-npm.sh ${var.repository_owner} ${var.source_name} ${var.source_version} ${var.package_name} ${var.package_type} ${local.tmp_dir}/${var.release_name}/${local.build_folder}/source-app.tgz"
   }
 
   # Unpack the tarball strip the top level directory: package/
   provisioner "local-exec" {
     command     = "tar --strip-components=1 -zxf source-app.tgz"
-    working_dir = "${path.root}/.work/${var.release_name}/${local.build_folder}/"
+    working_dir = "${local.tmp_dir}/${var.release_name}/${local.build_folder}/"
   }
 
   provisioner "local-exec" {
     command     = "rm -f source-app.tgz"
-    working_dir = "${path.root}/.work/${var.release_name}/${local.build_folder}/"
+    working_dir = "${local.tmp_dir}/${var.release_name}/${local.build_folder}/"
   }
 }
